78b4da693f4927a8eb2346f9b8ec1d6339874aab
DayanMonteiro <dmsmamb@gmail.com>
2021-11-29 08:59:47 -0300

    alterações no formulário

--- projeto-pim-8/projeto-pim-8/ViewModels/AboutViewModel.cs
+++ projeto-pim-8/projeto-pim-8/ViewModels/AboutViewModel.cs
@@ -9,7 +9,7 @@ namespace projeto_pim_8.ViewModels
     {
         public AboutViewModel()
         {
-            Title = "About";
+            Title = "Formulário";
             OpenWebCommand = new Command(async () => await Browser.OpenAsync("https://aka.ms/xamarin-quickstart"));
         }
 
